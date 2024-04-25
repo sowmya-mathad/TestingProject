2062d1ba225b70cacb8d5776bcbf7d1f1caf8dfd
Sowmya <sowmyag719@gmail.com>
2024-04-25 07:46:13 -0400

    fixed error

--- src/collectionTest/TestCollect.java
+++ src/collectionTest/TestCollect.java
@@ -7,8 +7,8 @@ import java.util.stream.Collectors;
 public class TestCollect {
     public static void main(String args[]) {
         List<Integer> intList = Arrays.asList(12,45,67,78,889,343,22,56,76,78,98,23,5,8,8);
-        List<Integer> oddLst = intList.stream().map(num -> num%2 != 0).collect(Collectors.toList());
-        List<Integer> evenList = intList.stream().map(num -> num%2 == 0).collect(Collectors.toList());
+        List<Integer> oddLst = intList.stream().filter(num -> num%2 != 0).collect(Collectors.toList());
+        List<Integer> evenList = intList.stream().filter(num -> num%2 == 0).collect(Collectors.toList());
 
     }
 }
