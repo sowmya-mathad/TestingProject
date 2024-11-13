9ef203df0bfdd84df2ab2164485e1dec46973767
Sowmya <sowmyag719@gmail.com>
2024-11-13 06:05:06 +0530

    understanding Java 17 and 21 features

--- src/java17And21/TemplateProcessorTest.java
+++ src/java17And21/TemplateProcessorTest.java
@@ -1,18 +1,47 @@
 package java17And21;
 
-/** Uncomment the line 4/5 while running the file**/
+/**
+ * Uncomment the line 6 while running the file
+ **/
 //import static java.lang.StringTemplate.STR;
-//import static java.lang.StringTemplate.RAW;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalTime;
 
-
-/** Execute on command line with the command
- * java --source 21 --enable-preview AbsolutePath of file TemplateProcessorTest.java*/
+/**
+ * Execute on command line with the command
+ * java --source 21 --enable-preview AbsolutePath of file TemplateProcessorTest.java
+ */
 public class TemplateProcessorTest {
-    public static void main(String[] args) {
-        String name = "JavaDZone";
+    private static String name = "JavaDZone";
+    private String course = "Java21 Features";
+    private static int a = 100;
+    private static int b = 200;
 
-/** Uncomment the line 14/15 while running the file**/
-//        System.out.println(STR."Welcome to \{name}");
-//        System.out.println(RAW."Welcome to \{name}.");
+    public static void main(String[] args) {
+
+        // Using variable in template expression. uncomment below line
+       //System.out.println(STR."Welcome to \{name}");
+
+        // Utilizing a method in the template expression. uncomment below line
+       // System.out.println(STR."Welcome to \{getName()}");
+
+
+        TemplateProcessorTest st = new TemplateProcessorTest();
+
+        // Using non-static variable in the template expression. uncomment below line
+      //  System.out.println(STR."Welcome to \{st.course}");
+
+        // Performing arithmetic operations within the expression. uncomment below line
+     //   System.out.println(STR."\{a} + \{b} = \{a+b}");
+
+        // Displaying the current date using expression uncomment below line
+      //  System.out.println(STR."The current date is: \{DateTimeFormatter.ofPattern("yyyy-MM-dd").format(LocalDateTime.now())}");
+
+    }
+
+
+    public static String getName() {
+        return name;
     }
 }
