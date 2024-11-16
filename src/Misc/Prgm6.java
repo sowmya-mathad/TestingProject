9d1bc180518a4ba1a4b30825b67c7ba4f735b349
Sowmya <sowmyag719@gmail.com>
2024-11-16 12:44:46 +0530

    UST interview programs

---
+++ src/Misc/Prgm6.java
@@ -0,0 +1,14 @@
+package Misc;
+
+
+/** 10 factorial num using java 8*/
+
+public class Prgm6 {
+    public static void main(String[] args) {
+        Integer n = 101;
+        if(n%10 == 0)
+            System.out.println("is factorial of 10");
+        else
+            System.out.println("Not factorial");
+    }
+}
